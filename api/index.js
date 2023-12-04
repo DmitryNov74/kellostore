8df926c3da3e8884ca6e415f4237cb1136064314
Dmitry <dm.developer@gmail.com>
2023-12-04 10:59:49 +0200

    middleware added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,7 +16,20 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5002, () => {
   console.log('serveri on käynnissä portilla 5002');
+});
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'internal server error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
